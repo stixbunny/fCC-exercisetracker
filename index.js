b6ba5b3c09760c1d55860b4643b569ba94a59e7a
stixbunny <milenko.castillo@gmail.com>
2023-03-22 23:40:13 -0300

    Update index.js - basic setup

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
 require('dotenv').config()
+
+// MongoDB setup
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username: {type: String, required: true},
+    description: String,
+    duration: Number,
+    date: Date
+});
+
+const userSchema = new Schema({
+    username: {type: String, required: true}
+});
+
+const logSubSchema = new Schema({
+    description: String,
+    duration: Number,
+    date: Date
+});
+
+const logSchema = new Schema({
+    username: {type: String, required: true},
+    count: 1,
+    log: [ logSubSchema ]
+});
+//
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +38,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+// Creates a user
+app.post("/api/users", (req, res) => {
+    const userRecord = new userSchema({username: req.body.username});
+    userRecord.save((err, savedUser) => {
+        if (err) return console.error(err);
+        res.json({username: savedUser.username, _id: savedUser.id});
+    });
+});
+
+// Gets list of users
+app.get("/api/users", (req, res) => {
+
+});
+
+// Creates a Excersise
+app.post("/api/users/:_id/exercises", (req, res) => {
+
+});
+
+// Gets log of user
+app.get("/api/users/:_id/logs", (req, res) => {
+
+});
